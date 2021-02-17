1ab8e3b521302f53fdea243982a900239312d445
itheamc <itheamc@gmail.com>
2021-02-17 17:25:51 +0545

    Third Commit - Adding Required Stock data to the list

--- src/com/itheamc/jsoup/StockMarkets.java
+++ src/com/itheamc/jsoup/StockMarkets.java
@@ -1,11 +1,14 @@
 package com.itheamc.jsoup;
 
+import com.itheamc.jsoup.model.Data;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StockMarkets {
     private static final String INFO_PAGE_URL = "https://merolagani.com/CompanyDetail.aspx?symbol=NBL";
@@ -25,9 +28,22 @@ public class StockMarkets {
             Element infoTable = body.getElementById("accordion");
             Elements tableBodies = infoTable.select(".panel ");
 
+            // Sector Title
+            Element sectorTitle = tableBodies.get(0).getElementsByTag("td").get(0);
+            // Sector Value
+            Element sectorValue = tableBodies.get(0).getElementsByTag("tr").get(0);
 
-            System.out.println(companyName.text());
-            System.out.println(tableBodies.get(0).getElementsByTag("td").get(0).text());
+            // Extracting required data and added to the list
+            List<Data> dataList = new ArrayList<>();
+            for (int i = 0; i < tableBodies.size(); i++) {
+                Element tempElement = tableBodies.get(i).getElementsByTag("tr").get(0);
+                String title = tempElement.getElementsByTag("th").text();
+                String value = tempElement.getElementsByTag("td").text();
+                dataList.add(new Data(i+1, title, value));
+            }
+
+
+            System.out.println(dataList.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
---
+++ src/com/itheamc/jsoup/model/Data.java
@@ -0,0 +1,38 @@
+package com.itheamc.jsoup.model;
+
+public class Data {
+    private int id;
+    private String title;
+    private String value;
+
+    // COnstructor
+    public Data(int id, String title, String value) {
+        this.id = id;
+        this.title = title;
+        this.value = value;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+
+    // Overriding toString() method
+    @Override
+    public String toString() {
+        return "Data{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", value='" + value + '\'' +
+                '}';
+    }
+}
